95c785feddb475408bf11f6897e108d286be10e4
xtayqria <3088365902@qq.com>
2020-12-29 13:07:53 +0800

    Initial Commit

---
+++ ZJJLocationNotification/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ZJJLocationNotification
+//
+//  Created by admin on 2020/12/29.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ ZJJLocationNotification/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  ZJJLocationNotification
+//
+//  Created by admin on 2020/12/29.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
